162b370beda1ff2c09cc54c442ceb1d027b07e17
Oleksandr Bretsko <bretsko.oleksandr@gmail.com>
2019-08-31 16:34:24 +0300

    switched to Swift Package, and other improvements

---
+++ Greetings/Greetings/RootViewController.swift
@@ -0,0 +1,170 @@
+
+import UIKit
+import EMPageViewController
+
+class RootViewController: UIViewController, EMPageViewControllerDataSource, EMPageViewControllerDelegate {
+    
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var scrollToButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    var pageVC: EMPageViewController?
+    
+    var greetings = ["Hello!", "¡Hola!", "Salut!", "Hallo!", "Ciao!"]
+    var greetingColors: [UIColor] = [
+        UIColor(red: 108.0/255.0, green: 122.0/255.0, blue: 137.0/255.0, alpha: 1.0),
+        UIColor(red: 135.0/255.0, green: 211.0/255.0, blue: 124.0/255.0, alpha: 1.0),
+        UIColor(red: 34.0/255.0, green: 167.0/255.0, blue: 240.0/255.0, alpha: 1.0),
+        UIColor(red: 245.0/255.0, green: 171.0/255.0, blue: 53.0/255.0, alpha: 1.0),
+        UIColor(red: 214.0/255.0, green: 69.0/255.0, blue: 65.0/255.0, alpha: 1.0)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let currentViewController = self.viewController(at: 0)!
+        let pageVC = EMPageViewController(currentViewController)
+                
+        pageVC.dataSource = self
+        pageVC.delegate = self
+        
+        pageVC.select(currentViewController, .forward, animated: false)
+        
+        addChild(pageVC)
+        view.insertSubview(pageVC.view, at: 0)
+        // Insert the page controller view below the navigation buttons
+        pageVC.didMove(toParent: self)
+        
+        self.pageVC = pageVC
+    }
+    
+    
+    // MARK: - scroll / transition methods
+    
+    @IBAction func forward(_ sender: AnyObject) {
+        pageVC!.scrollForward(animated: true, completion: nil)
+    }
+    
+    @IBAction func reverse(_ sender: AnyObject) {
+        pageVC!.scrollBack(animated: true, completion: nil)
+    }
+    
+    @IBAction func scrollTo(_ sender: AnyObject) {
+        
+        let choiceViewController = UIAlertController(title: "Scroll To...", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        
+        let selectedIndex = index(ofAccessibilityElement: pageVC!.selectedVC as! GreetingViewController)
+        
+        for (index, viewControllerGreeting) in greetings.enumerated() {
+            
+            guard index != selectedIndex else {
+                continue
+            }
+            
+            let action = UIAlertAction(title: viewControllerGreeting, style: UIAlertAction.Style.default, handler: { (alertAction) in
+                
+                let vc = self.viewController(at: index)!
+                
+                let direction: NavigationDirection = index > selectedIndex ? .forward : .back
+                
+                self.pageVC!.select(vc, direction, animated: true)
+            })
+            
+            choiceViewController.addAction(action)
+        }
+        
+        let action = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+        choiceViewController.addAction(action)
+        
+        present(choiceViewController, animated: true, completion: nil)
+    }
+    
+    func viewController(at index: Int) -> GreetingViewController? {
+        
+        if greetings.count == 0 || index < 0 || index >= greetings.count {
+            return nil
+        }
+        
+        let vc = storyboard!.instantiateViewController(withIdentifier: "GreetingViewController") as! GreetingViewController
+        vc.greeting = greetings[index]
+        vc.color = greetingColors[index]
+        return vc
+    }
+    
+    func index(of viewController: GreetingViewController) -> Int? {
+        if let greeting = viewController.greeting {
+            return greetings.firstIndex(of: greeting)
+        }
+        return nil
+    }
+
+
+    // MARK: - EMPageViewController Data Source
+    
+    func em_getControllerBefore(_ pageVC: EMPageViewController, controller vc: UIViewController) -> UIViewController? {
+        
+        if let i = index(of: vc as! GreetingViewController) {
+            let prevVC = self.viewController(at: i - 1)
+            return prevVC
+        }
+        return nil
+    }
+    
+    func em_getControllerAfter(_ pageVC: EMPageViewController, controller vc: UIViewController) -> UIViewController? {
+        
+        if let i = index(of: vc as! GreetingViewController) {
+            let nextVC = self.viewController(at: i + 1)
+            return nextVC
+        }
+        return nil
+    }
+    
+    
+    //MARK: EMPageViewControllerDelegate
+    
+    func em_willStartScrollingFrom(_ pageVC: EMPageViewController, willStartScrollingFrom startVC: UIViewController,  destinationVC: UIViewController) {
+        
+        let startGreetingViewController = startVC as! GreetingViewController
+        let destinationGreetingViewController = destinationVC as! GreetingViewController
+        
+        //print("Will start scrolling from \(startGreetingViewController.greeting) to \(destinationGreetingViewController.greeting).")
+    }
+    
+    func em_isScrollingFrom(_ pageVC: EMPageViewController, isScrollingFrom startVC: UIViewController, destinationVC: UIViewController, progress: CGFloat) {
+        let startGreetingViewController = startVC as! GreetingViewController
+        let destinationGreetingViewController = destinationVC as! GreetingViewController
+        
+        // Ease the labels' alphas in and out
+        let absoluteProgress = abs(progress)
+        startGreetingViewController.label.alpha = pow(1 - absoluteProgress, 2)
+        destinationGreetingViewController.label.alpha = pow(absoluteProgress, 2)
+        
+        //print("Is scrolling from \(startGreetingViewController.greeting) to \(destinationGreetingViewController.greeting) with progress '\(progress)'.")
+    }
+    
+    func em_didFinishScrollingFrom(_ pageVC: EMPageViewController, didFinishScrollingFrom startVC: UIViewController?, destinationVC: UIViewController, transitionSuccessful: Bool) {
+        let startVC = startVC as! GreetingViewController?
+        let destinationVC = destinationVC as! GreetingViewController
+        
+        // If the transition is successful, the new selected view controller is the destination view controller.
+        // If it wasn't successful, the selected view controller is the start view controller
+        if transitionSuccessful {
+            
+            if index(of: destinationVC) == 0 {
+                reverseButton.isEnabled = false
+            } else {
+                reverseButton.isEnabled = true
+            }
+            
+            if index(of: destinationVC) == greetings.count - 1 {
+                forwardButton.isEnabled = false
+            } else {
+                forwardButton.isEnabled = true
+            }
+        }
+        
+        //print("Finished scrolling from \(startVC?.greeting) to \(destinationVC.greeting). Transition successful? \(transitionSuccessful)")
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # EMPageViewController
 
-![EMPageViewController Demo](https://github.com/emalyak/EMPageViewController/blob/master/greetings-demo.gif)
+![EMPageViewController Demo](https://github.com/bretsko/EMPageViewController/blob/master/greetings-demo.gif)
 
 ## A better page view controller for iOS
 EMPageViewController is a full replacement for UIPageViewController with the features and predictability you’ve always wanted from a page view controller.
@@ -11,41 +11,6 @@ EMPageViewController gives your delegate messages for every step of the page tra
 ### Convenient navigation methods
 In addition to the ability scroll to any view controller, you can also easily scroll to the next or previous page without gestures if your app has navigation buttons.
 
-### Written in Swift (with support for Objective-C)
-EMPageViewController is not a subclass of UIPageViewController. Instead, it's a subclass of UIViewController with a UIScrollView, written in Swift, and it has common sense delegate and data source methods that will make the development of your page-based iOS app a breeze.
-
-## Compatibility
-* Xcode 7.0+
-* Swift 3.0+
-* Objective-C compatible
-* iOS 7+
-* iPhone, iPad, and iPod Touch
-
-## Installation
-
-There are two ways to install. Please note that the *CocoaPods* method requires iOS 8+, whereas the *file include* method requires iOS 7+
-
-### CocoaPods
-To install using CocoaPods, specify the following in your `Podfile`:
-```ruby
-source 'https://github.com/CocoaPods/Specs.git'
-platform :ios, '8.0'
-use_frameworks!
-
-pod 'EMPageViewController'
-```
-
-### File include
-Simply include the file [EMPageViewController.swift](https://github.com/emalyak/EMPageViewController/blob/master/EMPageViewController/EMPageViewController.swift) into your project.
-
-## Example usage / Demo
-Learn how to use EMPageViewController in your project by cloning this repository and opening the Xcode project file in:
-* Swift: [Examples/Greetings](https://github.com/emalyak/EMPageViewController/blob/master/Examples/Greetings).
-* Objective-C: [Examples/Greetings-ObjC](https://github.com/emalyak/EMPageViewController/blob/master/Examples/Greetings-ObjC).
-
-The code for initializing EMPageViewController and implementing its delegate and data source is located in:
-* Swift: [RootViewController.swift](https://github.com/emalyak/EMPageViewController/blob/master/Examples/Greetings/Greetings/RootViewController.swift)
-* Objective-C: [RootViewController.m](https://github.com/emalyak/EMPageViewController/blob/master/Examples/Greetings-ObjC/Greetings-ObjC/RootViewController.m)
 
 ## Documentation
 Full documentation is available on [CocoaDocs](http://cocoadocs.org/docsets/EMPageViewController)
